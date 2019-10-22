dcbc15e2e79b69153cc41b1dc7a1c7954dd6295e
niuyao <niuyao501396574@163.com>
2019-10-22 09:35:42 +0800

    初始化项目

---
+++ niuyao/niuyao/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  niuyao
+//
+//  Created by niuyao on 2019/10/22.
+//  Copyright © 2019 joymates. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
